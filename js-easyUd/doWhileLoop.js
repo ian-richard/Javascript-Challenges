739823f296ce0852b826da77ed475284ba66c261
Ian F <ian.fiddeman@gmail.com>
2021-02-18 17:19:36 +0000

    doWhile

---
+++ js-easyUd/doWhileLoop.js
@@ -0,0 +1,12 @@
+//demonstrate a do-while loop. NB - do should always run once. 
+
+let i = 9;
+
+do {
+    if (i % 2 !== 0) console.log(i);
+    i++;
+    } while (i <= 5);
+
+// doWhileLoop(); 
+
+

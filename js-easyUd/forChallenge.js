f8a68c8d04bca766a41cd8c43b69783ce19bf077
Ian F <ian.fiddeman@gmail.com>
2021-02-18 00:38:34 +0000

    while looped added

--- js-easyUd/forChallenge.js
+++ js-easyUd/forChallenge.js
@@ -13,9 +13,20 @@ for (let i = num; i <= 10; i++){
 
 // printOdds(4);
 
-// decrement i
+// decrement i, odd numbers in the reverse order 
 function printOdds__decrement() {
     for (let i = 5; i >= 1; i--){
         if (i % 2 !==0) console.log(i)}};
     
-printOdds__decrement();
+// printOdds__decrement();
+
+// while loop version of for loop
+
+function printOddsWhile () {
+    let i = 0;
+    while ( i <= 10){
+        if (i % 2 !==0) console.log(i);
+    i++}};
+
+printOddsWhile();
+

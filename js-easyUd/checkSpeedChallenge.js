9ba30e2e502b55ff80db917c936c6c8ad76bdf79
Ian F <ian.fiddeman@gmail.com>
2021-02-22 00:58:53 +0000

    refactor checkspeed

--- js-easyUd/checkSpeedChallenge.js
+++ js-easyUd/checkSpeedChallenge.js
@@ -32,16 +32,15 @@ function checkSpeed(speed){
     const speedLimit = 70;
     const kmPerPoint = 5;
 
-    if (speed < speedLimit)
-    console.log('Ok');
-    else {
-        const points = Math.floor((speed - speedLimit) / kmPerPoint);
-        if (points >= 12)
-          console.log("Licence Suspended");
-        else 
-          console.log('Points ' + points);
+    if (speed <= speedLimit + kmPerPoint){
+        console.log('Ok');
+        return;
     }
-}
+    const points = Math.floor((speed - speedLimit) / kmPerPoint);
+    if (points >= 12)
+        console.log("Licence Suspended");
+    else 
+        console.log('Points ' + points);
+    }
 
 const test = checkSpeed(80);
-console.log(test);

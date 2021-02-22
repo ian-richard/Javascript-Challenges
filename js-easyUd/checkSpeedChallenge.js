41bc3f3942bd9d8c7744ee82dd0c92744dfc764c
Ian F <ian.fiddeman@gmail.com>
2021-02-22 00:52:29 +0000

    refactor checkspeed

--- js-easyUd/checkSpeedChallenge.js
+++ js-easyUd/checkSpeedChallenge.js
@@ -3,20 +3,45 @@
 // Math.floor takes a decimal it it's single greatest number. i.e 1.3 to 1. 
 // 12 points = licence suspended
 
-function checkSpeed(num){
-  let speed = Math.floor(num);
-  let points = 0
-  let sum =  speed - 70;
-  let calc = 0
-  if (speed <= 74)
-    return 'OK'
-  else if (speed >= 130)
-        return 'Licence suspended';
-  else if (
-    points = (sum / 5)
-  )
-    return Math.floor(points);
-};
 
-let test = checkSpeed(99);
+
+
+
+// function checkSpeed(num){
+//   const speedLimit = 74;
+//   const speedToSuspendLicence = 130;
+//   const mphPerPoint = 5;
+//   let speed = Math.floor(num);
+//   let points = 0
+//   let sum =  speed - 70;
+//   let calc = 0
+//   if (speed <= speedLimit)
+//     return 'OK'
+//   else if (speed >= speedToSuspendLicence)
+//         return 'Licence suspended';
+//   else if (
+//     points = (sum / mphPerPoint)
+//   )
+//     return 'Points: ' + Math.floor(points);
+// };
+
+// let test = checkSpeed(80);
+// console.log(test);
+
+function checkSpeed(speed){
+    const speedLimit = 70;
+    const kmPerPoint = 5;
+
+    if (speed < speedLimit)
+    console.log('Ok');
+    else {
+        const points = Math.floor((speed - speedLimit) / kmPerPoint);
+        if (points >= 12)
+          console.log("Licence Suspended");
+        else 
+          console.log('Points ' + points);
+    }
+}
+
+const test = checkSpeed(80);
 console.log(test);

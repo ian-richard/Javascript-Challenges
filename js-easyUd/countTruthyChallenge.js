0284336e8b1fff82a9769206f03147a1ce3d820f
Ian F <ian.fiddeman@gmail.com>
2021-02-22 14:21:37 +0000

    count truthy challenge added v2

--- js-easyUd/countTruthyChallenge.js
+++ js-easyUd/countTruthyChallenge.js
@@ -1,5 +1,16 @@
 
 
 const myArray = [0, null, undefined, '', 2, 3]
-const count = myArray.filter(Boolean);
-console.log(count.length);
+// const count = myArray.filter(Boolean);
+// console.log(count.length);
+
+function countTruthy(array){
+    let count = 0;
+    for (let value of array)
+      if (value)
+        count++;
+    return count;
+}
+
+const test1 = countTruthy(myArray);
+console.log(test1);

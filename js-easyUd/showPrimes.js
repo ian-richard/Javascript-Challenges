606d652c9c56d72eca2a81bb073163b65f81dac5
Ian F <ian.fiddeman@gmail.com>
2021-02-24 22:11:02 +0000

    showPrimes single responsibility version added

--- js-easyUd/showPrimes.js
+++ js-easyUd/showPrimes.js
@@ -1,7 +1,7 @@
 //create a function which logs all 
 //prime numbers upto a set number, i.e. the limit
 
-const test = showPrimes(10)
+// const test = showPrimes(10)
 
 function showPrimes(limit){
     for (let num = 2; num <= limit; num++){
@@ -16,3 +16,18 @@ function showPrimes(limit){
     }
 }
 
+//single responsibility version
+
+const test = showPrimes_single_responsibility(10);
+
+function showPrimes_single_responsibility(limit){
+    for (let num = 2; num <= limit; num++)
+        if (isPrime(num)) console.log(num);
+}
+
+function isPrime(num){
+    for(let factor = 2; factor < num; factor++)
+        if (num % factor === 0)
+            return false;
+        return true;
+}

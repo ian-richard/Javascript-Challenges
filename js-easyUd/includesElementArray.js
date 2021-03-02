727251fd3af7d5e2bead6730d47965b2cb68ad0b
Ian F <ian.fiddeman@gmail.com>
2021-03-02 00:37:13 +0000

    refactor

--- js-easyUd/includesElementArray.js
+++ js-easyUd/includesElementArray.js
@@ -1,9 +1,19 @@
 const numbers = [1, 2, 3, 4]
-const e = 9;
+const e = 1;
 
 function  includesEx(array, element) {
     return array.includes(element)};
 
+    //alternative 
+function includes(arr, element){
+    for (let v of arr)
+        if (v === element)
+            return true
+    return false
+}
+
+const test2 = includes(numbers, e);
+
 
 const test = includesEx(numbers, e);
-console.log(test);
+console.log(test2);

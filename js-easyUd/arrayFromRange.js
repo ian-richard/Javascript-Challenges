2df14772b4088ca04c15ef801071c0fdb3d4fa2a
Ian F <ian.fiddeman@gmail.com>
2021-03-02 00:30:01 +0000

    array includes element exercise added

--- js-easyUd/arrayFromRange.js
+++ js-easyUd/arrayFromRange.js
@@ -1,10 +1,18 @@
-const numbers = arrayFromRange(-2,1);
 
-var MIN=17,MAX=23 //--> [18,19,20,21,22,23] EXPECTED
+function arrayFromRange_loop(min, max){
+    let output = [];
+    for (let i = min; i <= max; i++)
+        output.push(i)
+        return output
+}
+
+const numbers = arrayFromRange_loop(-1,4);
 
 function arrayFromRange(MIN, MAX){
     return Array.from({length:MAX-MIN+1},(v,k)=>k+MIN)
 }
 
 console.log(numbers);
+
+
 
---
+++ js-easyUd/includesElementArray.js
@@ -0,0 +1,9 @@
+const numbers = [1, 2, 3, 4]
+const e = 9;
+
+function  includesEx(array, element) {
+    return array.includes(element)};
+
+
+const test = includesEx(numbers, e);
+console.log(test);

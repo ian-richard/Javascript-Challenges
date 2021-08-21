c50f707685f1dce19a61710aedc2e3ef5b4c2273
Ian F <ian.fiddeman@gmail.com>
2021-08-21 21:23:46 +0100

    refactor descendingOrder

--- JS/descendingOrder/app.js
+++ JS/descendingOrder/app.js
@@ -1,20 +1,8 @@
 module.exports = {
-  descendingOrder: function(n){
-   
-    var numsToString = n.toString()
-    // console.log(numsToString)
+ descendingOrder: function(n){
+    var result = n.toString().split('').sort(function(a,b){return b-a}).join('')
     
-    var splitString = numsToString.split('')
-    // console.log(splitString)
-    
-    var sortString = splitString.sort(function(a,b){return b-a})
-    // console.log(sortString)
-    
-    var join = sortString.join('')
-    // console.log(join)
-    
-    var result = parseInt(join);
-    
-    return result;
+    return parseInt(result)
+     
   }
 }

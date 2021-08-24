a70335c869b847be28e31ac3a192a1dd08ec7fa4
Ian F <ian.fiddeman@gmail.com>
2021-08-24 14:04:09 +0100

    advanced sol to getMiddle added

--- JS/GetMiddleChar/subStringSol.js
+++ JS/GetMiddleChar/subStringSol.js
@@ -7,3 +7,15 @@ const test = 'animal'
 const test_odd = 'animals'
 
 console.log(getMiddle(test_odd))
+
+function getMiddle__slice(s)
+{
+  return s.slice((s.length-1)/2, s.length/2+1);
+}
+
+function getMiddle__charAt(s) {
+  var middle = s.length / 2;
+  return (s.length % 2) 
+    ? s.charAt(Math.floor(middle))
+    : s.slice(middle - 1, middle + 1);
+}

39b86497d6998830eaf95f09e0538c19a2921487
Ian F <ian.fiddeman@gmail.com>
2021-08-24 22:20:29 +0100

    polygonal numbers challenge added

--- JS/findSmallestInteger/sol.js
+++ JS/findSmallestInteger/sol.js
@@ -2,9 +2,15 @@ const findSmallestInt = args =>{
       args.sort((a, b) => a - b  )
         return args[0]
       }  
+
+//or use Math.min
+
+const mathMinSol = (args) =>{
+    return Math.min(...args)
+  }
     
 const test = [34, 15, 88, 2];
 
 const test1 = [34, -345, -1, 100]
 
-console.log(findSmallestInt(test1))
+console.log(mathMinSol(test1))
---
+++ JS/sumOfOddNums_triangle.js/sol.js
@@ -0,0 +1,8 @@
+function rowSumOddNumbers(n) {
+    return n*n*n
+  }
+
+/*  The rows' start numbers are Hogben's centered polygonal numbers:
+     1, 3, 7, 13, 21, 31, 43 = b[n] = n^2 - n + 1.
+     <https://oeis.org/A002061>
+     */

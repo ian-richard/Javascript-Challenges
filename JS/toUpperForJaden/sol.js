c45fd84c9b9042bd9e48bb0726119c2f1b84eb67
Ian F <ian.fiddeman@gmail.com>
2021-08-24 18:43:56 +0100

    alt sol to Jaden Case added

--- JS/toUpperForJaden/sol.js
+++ JS/toUpperForJaden/sol.js
@@ -4,7 +4,13 @@ const toJaden = (str) => {
     
       
     }
+const toJaden_CharAt = str => {
+        return str.split(" ").map(function(word){
+          return word.charAt(0).toUpperCase() + word.slice(1);
+        }).join(" ");
+    }
 
 const nonJadenCase = "How can mirrors be real if our eyes aren't real"
 
 console.log(toJaden(nonJadenCase))
+console.log(toJaden_CharAt(nonJadenCase))

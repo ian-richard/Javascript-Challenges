f6b828f4add7620f1b48a77cf109809a97b74168
Ian F <ian.fiddeman@gmail.com>
2021-08-27 16:00:15 +0100

    Isogram chall added

---
+++ JS/isIsogram_regex/sol.js
@@ -0,0 +1,22 @@
+function isIsogram(str){
+    var result = true;
+    var withNoDigits = str.replace(/[0-9]/g, '');
+  //   if (withNoDigits == ''){ result = true;}
+    if (/^.*(.).*\1.*/gi.test(str)){
+      result = false;
+    
+    }
+    return result;
+  }
+
+//one-liner sol
+
+function isIsogram_OneLiner(str){ 
+    return !/(\w).*\1/i.test(str)
+  }
+
+const t1 = 'Dermatoglyphics' //true
+const t2 = 'isIsogram' //false
+
+console.log(isIsogram_OneLiner(t1))
+console.log(isIsogram_OneLiner(t2))

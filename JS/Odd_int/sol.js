bebda2612b6792e03a289b49120840e9fe0b6caa
Ian F <ian.fiddeman@gmail.com>
2021-09-03 23:12:07 +0100

    find odd int sol added, prob over complicated

---
+++ JS/Odd_int/sol.js
@@ -0,0 +1,20 @@
+function findOdd(arr) {
+    var result = 0;
+   
+       
+    const occurences = arr.reduce(function (acc, curr) {
+     return acc[curr] ? ++acc[curr] : acc[curr] = 1, acc}, {});
+   
+    for (const property in occurences){
+      if ( occurences[property] % 2 == 1 || occurences[property] == 1){
+        result = parseInt(property);
+      }
+    }
+     return result;
+    
+     }
+
+var arr1 = [20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5] //answer 5;
+var arr2 = [1,1,2,-2,5,2,4,4,-1,-2,5] //answer -1;
+
+console.log(findOdd(arr2));

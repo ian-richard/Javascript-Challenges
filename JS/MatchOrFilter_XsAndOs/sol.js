06c0abb75e9fbb2edc23c52d840484917ecdea6a
Ian F <ian.fiddeman@gmail.com>
2021-09-08 21:58:08 +0100

    x and o challenge added

---
+++ JS/MatchOrFilter_XsAndOs/sol.js
@@ -0,0 +1,34 @@
+function XO_og(str) {
+  var lower = str.toLowerCase();
+  var arr = Array.from(lower);
+  var oCount = arr.filter((letter) => letter == "o");
+  var xCount = arr.filter((letter) => letter == "x");
+  if (oCount.length == xCount.length) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function XO_match(str) {
+  let x = str.match(/x/gi);
+  let o = str.match(/o/gi);
+  return (x && x.length) === (o && o.length);
+}
+
+const XO = str => {
+    str = str.toLowerCase().split('');
+    return str.filter(x => x === 'x').length === str.filter(x => x === 'o').length;
+  }
+
+console.log(XO('xo')) //true);
+console.log(XO('XO')) //true);
+console.log(XO('xo0')) //true);
+console.log(XO("xxOo")) //,true);
+console.log(XO("")); //,true,'Empty string contains equal amount of x and o');
+console.log(XO("xxxxxoooxooo")); //,true);
+console.log(XO('abcdefghijklmnopqrstuvwxyz')) //true,'Alphabet contains equal amount of x and o');
+console.log(XO('xxxoo')) //,false);
+console.log(XO("xxxm")); //,false);
+console.log(XO("ooom")); //,false);
+console.log(XO("Oo")); //,false);
--- JS/Replace().DNA/sol.js
+++ JS/Replace().DNA/sol.js
@@ -1,10 +1,16 @@
-function DNAStrand(dna){
+function DNAStrand__og(dna){
     return dna.replace(/A/g, 't')
               .replace(/T/g, 'a')
               .replace(/C/g, 'g')
               .replace(/G/g, 'c')
               .toUpperCase();
     }
+
+var pairs = {'A':'T','T':'A','C':'G','G':'C'};
+
+function DNAStrand(dna){
+  return dna.split('').map(function(v){ return pairs[v] }).join('');
+}
 
     console.log(DNAStrand("ATTGC")) //"TAACG","String ATTGC is"
     console.log(DNAStrand("GTAT")) //"CATA","String GTAT is"

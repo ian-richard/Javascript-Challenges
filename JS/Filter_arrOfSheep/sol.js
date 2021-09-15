d1d82468375d1d30e455d4f284c1948157647a52
Ian F <ian.fiddeman@gmail.com>
2021-09-15 19:27:13 +0100

    love function chal added

--- JS/Filter_arrOfSheep/sol.js
+++ JS/Filter_arrOfSheep/sol.js
@@ -2,6 +2,14 @@ function countSheeps(arr) {
     return arr.filter(e => e === true).length;
   }
 
+
+//alt
+
+function countSheeps_v2(arrayOfSheeps) {
+    return arrayOfSheeps.filter(Boolean).length;
+  }
+
+
   var array1 = [true,  true,  true,  false,
     true,  true,  true,  true ,
     true,  false, true,  false,
--- JS/TernaryOperator_easy_odds/oddsOrEven.js
+++ JS/TernaryOperator_easy_odds/oddsOrEven.js
@@ -3,3 +3,19 @@ function even_or_odd(number) {
   }
 
 console.log(even_or_odd(10))
+
+//other example
+
+/*
+Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each. 
+If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
+
+Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't. */
+
+
+function lovefunc(flower1, flower2){
+  return flower1 % 2 !== flower2 % 2;
+}
+console.log("lovefunc: ")
+console.log(lovefunc(1,4) === true)
+console.log(lovefunc(2,2) === false)

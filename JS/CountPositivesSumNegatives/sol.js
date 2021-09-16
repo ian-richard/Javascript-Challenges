e7c4120cb5c057f44db9f424fb060a3bef5ceb6b
Ian F <ian.fiddeman@gmail.com>
2021-09-16 12:36:20 +0100

    count pos and negs added

--- JS/CountPositivesSumNegatives/sol.js
+++ JS/CountPositivesSumNegatives/sol.js
@@ -1,4 +1,4 @@
-function countPositivesSumNegatives(input) {
+function countPositivesSumNegatives_og(input) {
     var res = [];
     if (input == null){ return res}
       else {
@@ -9,4 +9,29 @@ function countPositivesSumNegatives(input) {
       }
   }
 
-  
+    //alt
+
+    function countPositivesSumNegatives(input) {
+        if (input == null || input.length == 0)
+          return [];
+        
+        var positive = 0;
+        var negative = 0;
+        
+        for (var i=0, l=input.length; i<l; ++i)
+        {
+          if (input[i] > 0)
+            ++ positive;
+          else
+            negative += input[i];
+        }
+        
+        return [positive, negative];
+    }
+
+  let testData = [0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14];
+  let actual = countPositivesSumNegatives(testData);
+
+  console.log('Result '+actual)
+  console.log('Expected [8, -50]') 
+

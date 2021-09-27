46146489565d49e289711bb5bea308f26e4ca1a6
Ian F <ian.fiddeman@gmail.com>
2021-09-27 09:20:25 +0100

    reverse seq chal added

---
+++ JS/ReverseStuff/ReverseSequenceNumsArray/sol.js
@@ -0,0 +1,24 @@
+/*
+Build a function that returns an array of integers from n to 1 where n>0.
+
+Example : n=5 --> [5,4,3,2,1]
+*/
+
+const reverseSeq_og = n => {
+    let arr = [];
+      for (let i=n; i>0; i--) {
+        arr.push(i);
+        } return arr;
+    };
+
+//alternatives
+
+const reverseSeq_odd = length => Array.from({length}, () => length--) //odd
+
+const reverseSeq = n => {
+    return Array(n).fill(0).map((e, i) => n - i );
+};
+
+
+
+console.log(reverseSeq(5)) // == [5, 4, 3, 2, 1])

229e6023c5e7b32d75faca68caaf4195e4926eed
Ian F <ian.fiddeman@gmail.com>
2021-10-21 22:23:43 +0100

    count sheep added

---
+++ JS/StringLiterals/countSheepForLoop.js
@@ -0,0 +1,22 @@
+// If you can't sleep, just count sheep!!
+// Task:
+
+// Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". 
+//Input will always be valid, i.e. no negative integers.
+
+
+
+var countSheep_og = function (num){
+    var res = "";
+    for (var i = 1; i <= num; i++){
+      res += `${i} sheep...`;
+    }
+    return res;
+  }
+
+  const countSheep = length =>
+  Array.from({ length }, (_, i) => ++i + ' sheep...').join('')
+
+console.log(countSheep(1) === "1 sheep...");
+console.log(countSheep(2) === "1 sheep...2 sheep...");
+console.log(countSheep(3) === "1 sheep...2 sheep...3 sheep...");

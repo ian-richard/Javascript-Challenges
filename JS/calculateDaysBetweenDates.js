8e4c8365d57b06f91d35e43f169f03fd6f4a83f4
Ian F <ian.fiddeman@gmail.com>
2021-11-04 19:19:26 +0000

    testing cp

---
+++ JS/calculateDaysBetweenDates.js
@@ -0,0 +1,11 @@
+
+
+function calculateDaysBetweenDates(begin, end) {
+    var oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+    var firstDate = new Date(begin);
+    var secondDate = new Date(end);
+    
+    return Math.round(Math.abs((firstDate.getTime() - secondDate.getTime()) / (oneDay)));
+}
+
+console.log(calculateDaysBetweenDates("08/05/2021", "11/04/2021")+1);
---
+++ JS/imagesWithoutAltText.js
@@ -0,0 +1,10 @@
+//find all imaeges without alternate text
+// and give them a red border
+function process(){
+    var images = document.getElementsByTagName('img');
+    for(var i = 0; i < images.length; i++){
+        if(images[i].alt == ''){
+            images[i].style.border = '2px solid red';
+        }
+    }
+}

fbacffb04cd271b1c681078b370d077a7b2c31c6
Ian F <ian.fiddeman@gmail.com>
2021-11-23 12:10:19 +0000

    bit counting added

--- JS/Numbers/MultiplyBigNumbersAsStrings.js
+++ JS/Numbers/MultiplyBigNumbersAsStrings.js
@@ -7,6 +7,7 @@
 //     Answer should be returned as a string
 //     The returned "number" should not start with zeros e.g. 0123 is invalid
 
+
 function multiply(a, b){
     return (BigInt(a) * BigInt(b)).toString();
   }

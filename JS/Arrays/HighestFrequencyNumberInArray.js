20f15c2e0a041c36cdafda3bbba2c65cc9ba65fa
Ian F <ian.fiddeman@gmail.com>
2021-11-23 10:40:59 +0000

    return the highest freq number in an array challenge added

---
+++ JS/Arrays/HighestFrequencyNumberInArray.js
@@ -0,0 +1,34 @@
+
+// create an object that has the number as the key and the frequency as the value
+// iterate through the array and add the number to the object
+// iterate through the object and find the highest frequency
+// return the highest frequency number
+
+const highestRank = (arr) => {
+    let obj = {};
+    for (let i = 0; i < arr.length; i++) {
+        if (!obj.hasOwnProperty(arr[i])) {
+            obj[arr[i]] = 1;
+        } else {
+            obj[arr[i]] += 1;
+        }
+    }  
+     
+    console.log(obj)
+    
+    let highestFrequency = 0;
+    let highestFrequencyNumber = 0;
+    
+    for (let key in obj) {
+        if (obj[key] > highestFrequency) {
+            highestFrequency = obj[key];
+            highestFrequencyNumber = key;
+        }   
+    }
+    
+    return parseInt(highestFrequencyNumber);
+    
+}
+
+var arr = [12, 10, 8, 12, 7, 6, 4, 10, 12];
+console.log(highestRank(arr) === 12);
